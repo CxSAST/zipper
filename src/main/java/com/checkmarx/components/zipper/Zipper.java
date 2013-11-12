9d86b2fc4ee7bb69b43042668db673f631d53870
Denis Krivitski <denis.krivitski@gmail.com>
2013-11-12 18:34:24 +0200

    Zip file skeleton created

--- src/main/java/com/checkmarx/components/zipper/Zipper.java
+++ src/main/java/com/checkmarx/components/zipper/Zipper.java
@@ -1,11 +1,16 @@
 package com.checkmarx.components.zipper;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.tools.ant.DirectoryScanner;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.LinkedList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,19 +22,123 @@ import java.util.LinkedList;
  *  and archive all files passing the specified filter test. The filter string is a comma separated list
  *  of include and exclude patterns.
  *
+ *
+ * Pattern Syntax: (taken from DirectoryScanner documentation)
+ *
+ * A given directory is recursively scanned for all files
+ * and directories. Each file/directory is matched against a set of selectors,
+ * including special support for matching against filenames with include and
+ * and exclude patterns. Only files/directories which match at least one
+ * pattern of the include pattern list or other file selector, and don't match
+ * any pattern of the exclude pattern list or fail to match against a required
+ * selector will be placed in the list of files/directories found.
+ * <p>
+ * When no list of include patterns is supplied, "**" will be used, which
+ * means that everything will be matched. When no list of exclude patterns is
+ * supplied, an empty list is used, such that nothing will be excluded. When
+ * no selectors are supplied, none are applied.
+ * <p>
+ * The filename pattern matching is done as follows:
+ * The name to be matched is split up in path segments. A path segment is the
+ * name of a directory or file, which is bounded by
+ * <code>File.separator</code> ('/' under UNIX, '\' under Windows).
+ * For example, "abc/def/ghi/xyz.java" is split up in the segments "abc",
+ * "def","ghi" and "xyz.java".
+ * The same is done for the pattern against which should be matched.
+ * <p>
+ * The segments of the name and the pattern are then matched against each
+ * other. When '**' is used for a path segment in the pattern, it matches
+ * zero or more path segments of the name.
+ * <p>
+ * There is a special case regarding the use of <code>File.separator</code>s
+ * at the beginning of the pattern and the string to match:<br>
+ * When a pattern starts with a <code>File.separator</code>, the string
+ * to match must also start with a <code>File.separator</code>.
+ * When a pattern does not start with a <code>File.separator</code>, the
+ * string to match may not start with a <code>File.separator</code>.
+ * When one of these rules is not obeyed, the string will not
+ * match.
+ * <p>
+ * When a name path segment is matched against a pattern path segment, the
+ * following special characters can be used:<br>
+ * '*' matches zero or more characters<br>
+ * '?' matches one character.
+ * <p>
+ * Examples:
+ * <p>
+ * "**\*.class" matches all .class files/dirs in a directory tree.
+ * <p>
+ * "test\a??.java" matches all files/dirs which start with an 'a', then two
+ * more characters and then ".java", in a directory called test.
+ * <p>
+ * "**" matches everything in a directory tree.
+ * <p>
+ * "**\test\**\XYZ*" matches all files/dirs which start with "XYZ" and where
+ * there is a parent directory called test (e.g. "abc\test\def\ghi\XYZ123").
+ * <p>
+ * Case sensitivity may be turned off if necessary. By default, it is
+ * turned on.
+ * <p>
+ * Example of usage:
+ * <pre>
+ *   String[] includes = {"**\\*.class"};
+ *   String[] excludes = {"modules\\*\\**"};
+ *   ds.setIncludes(includes);
+ *   ds.setExcludes(excludes);
+ *   ds.setBasedir(new File("test"));
+ *   ds.setCaseSensitive(true);
+ *   ds.scan();
+ *
+ *   System.out.println("FILES:");
+ *   String[] files = ds.getIncludedFiles();
+ *   for (int i = 0; i < files.length; i++) {
+ *     System.out.println(files[i]);
+ *   }
+ * </pre>
+ * This will scan a directory called test for .class files, but excludes all
+ * files in all proper subdirectories of a directory called "modules"
+ *
  */
 public class Zipper {
 
     private static final Logger logger = Logger.getLogger(Zipper.class);
 
-    public byte[] zip(File baseDir, String filterPatterns)
+    // TODO: add zip size limitation parameter
+    public void zip(File baseDir, String filterPatterns, OutputStream outputStream) throws IOException
     {
         DirectoryScanner ds = createDirectoryScanner(baseDir,filterPatterns);
         ds.scan();
         printDebug(ds);
 
 
-        return null;
+        zipFile(ds.getIncludedFiles(),outputStream);
+
+
+        return;
+    }
+
+    /*
+        Convenience method for receiving the zip archive in byte array
+     */
+
+    public byte[] zip(File baseDir, String filterPatterns) throws IOException {
+        ByteOutputStream byteOutputStream = new ByteOutputStream();
+        zip(baseDir,filterPatterns,byteOutputStream);
+        return byteOutputStream.getBytes();
+    }
+
+    private void zipFile(String[] files, OutputStream outputStream) throws IOException {
+        ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream);
+        for(String file : files)
+        {
+            logger.debug("Adding file to zip: " + file);
+            ZipEntry zipEntry = new ZipEntry(file);
+            zipOutputStream.putNextEntry(zipEntry);
+            zipOutputStream.write(new byte[]{1,2,3});
+            zipOutputStream.closeEntry();
+        }
+
+        zipOutputStream.close();
     }
 
 
--- src/test/java/com/checkmarx/components/zipper/ZipperTest.java
+++ src/test/java/com/checkmarx/components/zipper/ZipperTest.java
@@ -5,6 +5,8 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -37,7 +39,11 @@ public class ZipperTest extends TestCase {
 
         Zipper zipper = new Zipper();
         String filters = "**, !**/f1.*\n!**/f2.*, !* *   ,/pr,pr/, ! .DS_Store,  !.DS_Store, !! .DS_Store,,,   ,\" \" ";
-        byte[] zip = zipper.zip(new File("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/temp/basedir"),filters);
+        try {
+            byte[] zip = zipper.zip(new File("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/temp/basedir"),filters);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         assertTrue(true);
     }
@@ -47,7 +53,46 @@ public class ZipperTest extends TestCase {
 
         Zipper zipper = new Zipper();
         String filters = "!**/*.txt";
-        byte[] zip = zipper.zip(new File("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/temp/basedir"),filters);
+        try {
+            byte[] zip = zipper.zip(new File("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/temp/basedir"),filters);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(true);
+    }
+
+
+    public void testPattern3() {
+
+
+        Zipper zipper = new Zipper();
+        String filters = "a/*.txt";
+        try {
+            byte[] zip = zipper.zip(new File("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/temp/basedir"),filters);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(true);
+    }
+    public void testZip1() {
+
+
+        Zipper zipper = new Zipper();
+        String filters = "!f1.txt";
+        try {
+            byte[] zip = zipper.zip(new File("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/temp/basedir"),filters);
+            File outFile = new File("testOutput.zip");
+            System.out.println("Output file: " + outFile.getAbsolutePath());
+            FileOutputStream out = new FileOutputStream("work/testOutput.zip");
+            out.write(zip);
+            out.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         assertTrue(true);
     }
