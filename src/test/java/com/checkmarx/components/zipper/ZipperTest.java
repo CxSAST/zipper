38abab457d0b76b901b29ce7f3b13ea32eb2ce02
DM\Sergeyk <sergey.kadaner@checkmarx.com>
2015-12-22 17:02:07 +0200

    Cleanup

--- src/test/java/com/checkmarx/components/zipper/ZipperTest.java
+++ src/test/java/com/checkmarx/components/zipper/ZipperTest.java
@@ -1,167 +1,142 @@
 package com.checkmarx.components.zipper;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.notification.RunListener.ThreadSafe;
 
 /**
- * Created with IntelliJ IDEA.
- * User: denis
- * Date: 12/11/2013
- * Time: 15:52
- * Description:
+ * Created with IntelliJ IDEA. User: denis Date: 12/11/2013 Time: 15:52 Description:
  */
-public class ZipperTest extends TestCase {
+@ThreadSafe
+public class ZipperTest {
 
+	String root;
+	String output;
 
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public ZipperTest(String testName) {
-        super(testName);
-    }
+	@Before
+	public void setup() {
+		String dir = getClass().getProtectionDomain().getCodeSource().getLocation().getFile();
+		String projectRoot = dir.substring(0, dir.indexOf("build"));
+		root = projectRoot + "/src";
+		output = projectRoot + "/build/output" + UUID.randomUUID() + ".zip";
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(ZipperTest.class);
-    }
+	@Test
+	public void OfftestPattern1() throws IOException {
 
-    public void OfftestPattern1() {
+		Zipper zipper = new Zipper();
+		String filters = "**, !**/f1.*\n!**/f2.*, !* *   ,/pr,pr/, ! .DS_Store,  !.DS_Store, !! .DS_Store,,,   ,\" \" ";
 
+		byte[] zip = zipper.zip(new File(root), filters, 0, null);
+	}
 
-        Zipper zipper = new Zipper();
-        String filters = "**, !**/f1.*\n!**/f2.*, !* *   ,/pr,pr/, ! .DS_Store,  !.DS_Store, !! .DS_Store,,,   ,\" \" ";
-        try {
-            byte[] zip = zipper.zip(new File("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/temp/basedir"),filters,0,null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	@Test
+	public void OfftestPattern2() throws IOException {
 
-        assertTrue(true);
-    }
+		Zipper zipper = new Zipper();
+		String filters = "!***/*//*.txt";
 
-    public void OfftestPattern2() {
+		byte[] zip = zipper.zip(new File(root), filters, 0, null);
+	}
 
+	@Test(expected = IOException.class)
+	public void OfftestPattern3() throws IOException {
+		Zipper zipper = new Zipper();
+		String filters = "a*//*.txt";
 
-        Zipper zipper = new Zipper();
-        String filters = "!***/*//*.txt";
-        try {
-            byte[] zip = zipper.zip(new File("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/temp/basedir"),filters,0,null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		byte[] zip = zipper.zip(new File("root"), filters, 0, null);
+	}
 
-        assertTrue(true);
-    }
+	@Test
+	public void OfftestZip1() throws IOException {
 
+		Zipper zipper = new Zipper();
+		String filters = "!f1.txt";
 
-    public void OfftestPattern3() {
+		ZipListener zipListener = new ZipListener() {
+			@Override
+			public void updateProgress(String currentFileName, long currentCompressedSize) {
+				System.out.println("ZipListener.updateProgress: " + currentFileName + " size: " + currentCompressedSize);
+			}
+		};
 
+		FileOutputStream out = null;
+		try {
+			out = new FileOutputStream(output);
+			zipper.zip(new File(root), filters, out, 100000, zipListener);
 
-        Zipper zipper = new Zipper();
-        String filters = "a*//*.txt";
-        try {
-            byte[] zip = zipper.zip(new File("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/temp/basedir"),filters,0,null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
 
-        assertTrue(true);
-    }
-    public void OfftestZip1() {
+	}
 
+	@Test
+	@Ignore
+	public void testZip2() throws IOException {
 
-        Zipper zipper = new Zipper();
-        String filters = "!f1.txt";
+		Zipper zipper = new Zipper();
+		String filters = "!**/*.mov";
 
-        ZipListener zipListener = new ZipListener() {
-            public void updateProgress(String currentFileName, long currentCompressedSize) {
-                System.out.println("ZipListener.updateProgress: " + currentFileName + " size: " + currentCompressedSize);
-            }
-        };
+		ZipListener zipListener = new ZipListener() {
+			@Override
+			public void updateProgress(String currentFileName, long currentCompressedSize) {
+				System.out.println("ZipListener.updateProgress: " + currentFileName + " size: " + currentCompressedSize);
+			}
+		};
 
-        try {
-            FileOutputStream out = new FileOutputStream("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/zipper/target/work/testOutput.zip");
-            zipper.zip(new File("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/temp/basedir"),filters,out,100000,zipListener);
-            out.close();
+		FileOutputStream out = null;
+		try {
+			byte[] outArray = zipper.zip(new File(root), filters, 0, zipListener);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			out = new FileOutputStream(output);
+			out.write(outArray);
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
 
+	}
 
-        assertTrue(true);
-    }
+	@Test
+	public void OfftestErrors1() throws IOException {
 
-    public void testZip2() {
+		Zipper zipper = new Zipper();
+		String filters = "!f1.txt";
 
+		ZipListener zipListener = new ZipListener() {
+			@Override
+			public void updateProgress(String currentFileName, long currentCompressedSize) {
+				System.out.println("ZipListener.updateProgress: " + currentFileName + " size: " + currentCompressedSize);
+			}
+		};
+		FileOutputStream out = null;
+		try {
+			out = new FileOutputStream(output);
+			zipper.zip(new File(root), filters, out, 0, zipListener);
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
 
-        Zipper zipper = new Zipper();
-        String filters = "!**/*.mov";
+	}
 
-        ZipListener zipListener = new ZipListener() {
-            public void updateProgress(String currentFileName, long currentCompressedSize) {
-                System.out.println("ZipListener.updateProgress: " + currentFileName + " size: " + currentCompressedSize);
-            }
-        };
+	@Test
+	public void testBrokenLink() throws IOException {
+		Zipper zipper = new Zipper();
+		File baseDir = new File(root);
 
-        try {
-            byte[] outArray = zipper.zip(new File("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/temp/basedir"),filters,0,zipListener);
+		byte[] out = zipper.zip(baseDir, "", -1, null);
 
-            FileOutputStream out = new FileOutputStream("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/zipper/target/work/testOutput2.zip");
-            out.write(outArray);
-            out.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        assertTrue(true);
-    }
-
-    public void OfftestErrors1() {
-
-
-        Zipper zipper = new Zipper();
-        String filters = "!f1.txt";
-
-        ZipListener zipListener = new ZipListener() {
-            public void updateProgress(String currentFileName, long currentCompressedSize) {
-                System.out.println("ZipListener.updateProgress: " + currentFileName + " size: " + currentCompressedSize);
-            }
-        };
-
-        try {
-            FileOutputStream out = new FileOutputStream("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/zipper/target/work/testOutput.zip");
-            zipper.zip(new File("/Users/denis/Documents/iOSDevMac/Checkmarx/Zipper/temp/basedir"),filters,out,0,zipListener);
-            out.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        assertTrue(true);
-    }
-
-    public void testBrokenLink() {
-        Zipper zipper = new Zipper();
-        File baseDir = new File("/Users/denis/Documents/iOSDevMac/Checkmarx/IntelliJ PlugIn/Scanee Project 2/src");
-        try {
-            byte[] out = zipper.zip(baseDir,"",-1,null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
+	}
 }
-
